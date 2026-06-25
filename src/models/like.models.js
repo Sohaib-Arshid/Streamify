f6e319ef073603b7667c4830595b411cf78484db
Sohaib Arshid <sohaibali1277@gmail.com>
2026-06-25 08:58:39 +0500

    Implement like feature for videos: add like functionality and update Like model

--- src/models/like.models.js
+++ src/models/like.models.js
@@ -2,15 +2,17 @@ import mongoose, { Schema } from "mongoose";
 
 const likeSchema = Schema({
     user: {
-        type: "Schema.Types.ObjectId",
-        ref: "User"
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        unique : true
     },
     video: {
-        type: "Schema.Types.ObjectId",
-        ref: "Video"
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+        unique : true
     }
 },
-    { timestamps: true }, { unique: true }
+    { timestamps: true }
 )
 
 export const Like = mongoose.model("Like" , likeSchema)
